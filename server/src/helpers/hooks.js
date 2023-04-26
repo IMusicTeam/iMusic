e5bd4f99c401e7aa4e447b9a9969f8b5d82ef0fd
taja <taja@ideyalabs.com>
2023-04-26 13:10:16 +0530

    Song upload & edit & update API's were updated

--- server/src/helpers/hooks.js
+++ server/src/helpers/hooks.js
@@ -1,1 +1,8 @@
 // functions
+
+const { codes, strings } = require("../Constants");
+
+function serverDown(res) {
+    return  res.status(codes.serverError).json({ message: strings.serverError });
+}
+module.exports = {serverDown};
---
+++ server/src/middleWare/uploads.js
@@ -0,0 +1,26 @@
+const path = require("path")
+const multer = require("multer")
+
+const storage = multer.diskStorage({
+    filename: function (req, file, cb) {
+        let ext = path.extname(file.originalname)
+        cb(null, Date.now() + ext)
+    }
+})
+
+const upload = multer({
+    storage: storage,
+    fileFilter: function (req, file, cb) {
+        if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg" ||   file.mimetype == "audio/mpeg" || file.mimetype == "audio/vnd.wav" || file.mimetype == "audio/basic") {
+            cb(null, true)
+        } else {
+            cb(null, false)
+        }
+    },
+    limits: {
+        fileSize: 1024 * 1024 * 2
+    }
+
+})
+
+module.exports = upload
