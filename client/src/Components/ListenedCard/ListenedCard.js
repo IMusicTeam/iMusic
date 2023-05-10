bb27252e67d65067ffbc6b2e9d80b0fd28dc1f8d
Vennela-00 <VennelaP@ideyalabs.com>
2023-05-10 18:44:40 +0530

    Developed AlbumView Page

--- client/src/Components/ListenedCard/ListenedCard.js
+++ client/src/Components/ListenedCard/ListenedCard.js
@@ -1,8 +1,11 @@
 import React from "react";
+import { useNavigate } from "react-router";
 
 function ListenedCard(data) {
+  const navigateTo =useNavigate();
+
   return (
-    <div className="relative transition duration-300 ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 min-w-[360px] min-h-[360px] ">
+    <div onClick = {()=>navigateTo("/albumsView")}className="relative transition duration-300 ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 min-w-[360px] min-h-[360px] ">
       <img
         src={data.image}
         alt="card"
